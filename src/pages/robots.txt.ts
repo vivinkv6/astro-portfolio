547acb2cfb459caeb6cd0f017e1a6b646776c930
vivinkv6 <vivinkv0@gmail.com>
2026-03-22 18:38:01 +0530

    remove no index tag, update robots.txt

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -1,6 +1,6 @@
 import type { APIRoute } from "astro";
 
 export const GET: APIRoute = () =>
-  new Response(`User-agent: *\nDisallow: /\n`, {
+  new Response(`User-agent: *\nAllow: /\nSitemap: ${new URL("/sitemap.xml", import.meta.env.SITE || "https://vivinkv.me").toString()}\n`, {
     headers: { "Content-Type": "text/plain; charset=utf-8" }
   });

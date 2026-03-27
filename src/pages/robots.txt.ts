19c0f29c5c8c8c62a35061751724c767ae800c5c
vivinkv6 <vivinkv0@gmail.com>
2026-03-27 19:03:10 +0530

    update robots.txt

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -1,6 +1,8 @@
 import type { APIRoute } from "astro";
+export const siteUrl = import.meta.env.PUBLIC_SITE_URL || "http://localhost:4321";
+
 
 export const GET: APIRoute = () =>
-  new Response(`User-agent: *\nAllow: /\nSitemap: ${new URL("/sitemap.xml", import.meta.env.SITE || "https://vivinkv.me").toString()}\n`, {
+  new Response(`User-agent: *\nAllow: /\nSitemap: ${siteUrl}/sitemap.xml\n`, {
     headers: { "Content-Type": "text/plain; charset=utf-8" }
   });

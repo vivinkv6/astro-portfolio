a2435f55c3b299ebb1eddb8c948a98a55b13cf07
vivinkv6 <vivinkv0@gmail.com>
2026-03-28 11:29:02 +0530

    add 301 redirect and self host font

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,11 @@ import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  redirects: {
+    "/blogs/8452068674138656619": "/blogs/best-headless-cms-platforms-modern-web-development",
+    "/blogs/3935986206409354173": "/blogs/top-ai-coding-tools-ides-developer-productivity-2025",
+    "/blogs/7407633909543796871": "/blogs/from-classroom-to-code-pandemic-programming-journey"
+  },
   vite: {
     plugins: [tailwindcss()],
     resolve: {
